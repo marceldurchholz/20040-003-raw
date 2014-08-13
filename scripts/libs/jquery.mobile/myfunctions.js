c90e6ed5a5b0b35e20557bdb3e16330905ce398d
DigitalVerve <info@digitalverve.de>
2014-08-13 14:49:54 +0200

    13.08.2014_14:49:54,44

--- scripts/libs/jquery.mobile/myfunctions.js
+++ scripts/libs/jquery.mobile/myfunctions.js
@@ -269,11 +269,11 @@ function correctPageSize() {
 	$.mobile.activePage.parents('html').attr("style","height:300px !important;overflow-y:hidden !important;position:fixed !important;width:100% !important;height:100% !important;");
 	$.mobile.activePage.parents('body').attr("style","height:300px !important;overflow-y:hidden !important;position:fixed !important;width:100% !important;height:100% !important;");
 	$.mobile.activePage.parents('html').addClass("fixedHeightHtml");
-	$.mobile.activePage.parents('#container').attr("style","height:300px !important;overflow-y:hidden !important;position:fixed !important;width:100% !important;height:100% !important;");
+	$.mobile.activePage.parents('#container').attr("style","height:300px !important;overflow-y:hidden !important;position:fixed !important;width:100% !important;height:100% !important;margin:0px !important;");
 	if (window.pageHeight==undefined) window.pageHeight = parseInt($(document).height(),0)-100-46-16;
 	if (window.contentHeight==undefined) window.contentHeight = parseInt($(document).height(),0)-100-46-16;
 	$.mobile.activePage.parents('#container').find('.ui-page-active').attr("style","height:"+(window.pageHeight)+"px !important;overflow-y:hidden !important;");
-	$.mobile.activePage.parents('#container').find('.ui-page-active').find('.ui-content').attr("style","height:"+window.contentHeight+"px !important;overflow-y:auto;padding:0px !important;top:46px !important;");
+	$.mobile.activePage.parents('#container').find('.ui-page-active').find('.ui-content').attr("style","height:"+window.contentHeight+"px !important;overflow-y:auto;padding:0px 16px !important;top:46px !important;");
 	$.mobile.silentScroll(0);
 	// alert(contentHeight);
 	// $('#container').parent().parent().find('#body').attr("style","height:300px !important;overflow-y:hidden;");
