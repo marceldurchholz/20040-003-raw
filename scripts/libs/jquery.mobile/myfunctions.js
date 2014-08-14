b1b9d7891c60faa46745765833861a3161df8299
DigitalVerve <info@digitalverve.de>
2014-08-14 22:20:21 +0200

    14.08.2014_22:20:20,86

--- scripts/libs/jquery.mobile/myfunctions.js
+++ scripts/libs/jquery.mobile/myfunctions.js
@@ -285,7 +285,10 @@ function correctPageSize() {
 	if (window.contentHeight==undefined || window.last_footerFixHeight!=window.footerFixHeight) window.contentHeight = parseInt($(document).height(),0)-window.footerFixHeight-46-16;
 	$.mobile.activePage.parents('#container').find('.ui-page-active').attr("style","height:"+(window.pageHeight)+"px !important;overflow-y:hidden !important;");
 	$.mobile.activePage.parents('#container').find('.ui-page-active').find('.ui-content').attr("style","height:"+window.contentHeight+"px !important;overflow-y:scroll !important;padding:0px 16px !important;top:46px !important;");
+	$.mobile.activePage.parents('#container').find('.ui-page-active').find('.ui-footer').attr("style","height:100px !important;max-height:100px !important;margin:0px !important;padding:0px !important;width:100% !important;overflow:hidden !important;position:fixed !important;bottom:0px !important;left:0px !important;right:0px !important;vertical-align: middle !important;z-index:999 !important;");
 	$.mobile.silentScroll(0);
+	
+	
 	
 	window.last_footerFixHeight = window.footerFixHeight;
 	
