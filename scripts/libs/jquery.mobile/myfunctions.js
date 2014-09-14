9a25d95f0525d100a6617e58ff5a482ed18b18e3
DigitalVerve <info@digitalverve.de>
2014-09-14 18:44:17 +0200

    14.09.2014_18:44:16,85

--- scripts/libs/jquery.mobile/myfunctions.js
+++ scripts/libs/jquery.mobile/myfunctions.js
@@ -556,19 +556,19 @@ function disablescrolling() {
 	var breite = "300";
 	var hoehe = "300";
 	// alert(breite);
+	$.mobile.activePage.find('.ui-content').removeAttr('style');
+	$.mobile.activePage.removeAttr('style');
 	$.mobile.activePage.parents('html').addClass('disablescrolling');
 	$.mobile.activePage.parents('body').addClass('disablescrolling');
 	$.mobile.activePage.parents('container').addClass('disablescrolling');
-	// $.mobile.activePage.find('.ui-content').removeAttr('style');
-	// $.mobile.activePage.removeAttr('style');
 	$.mobile.activePage.find('.ui-page').addClass('disablescrolling');
 	// $.mobile.activePage.parent().find('.ui-page').css('width',$.mobile.activePage.parent().find('.ui-page').width()+'px');
 	// $.mobile.activePage.parent().find('.ui-page').css('height',$.mobile.activePage.parent().find('.ui-page').width()+'px');
-	// $.mobile.activePage.find('.ui-page').css('width',breite+'px');
-	// $.mobile.activePage.find('.ui-page').css('height',hoehe+'px');
+	$.mobile.activePage.find('.ui-page').css('width',breite+'px');
+	$.mobile.activePage.find('.ui-page').css('height',hoehe+'px');
 	$.mobile.activePage.find('.ui-content').addClass('disablescrolling');
-	// $.mobile.activePage.find('.ui-content').css('width',breite+'px');
-	// $.mobile.activePage.find('.ui-content').css('height',hoehe+'px');
+	$.mobile.activePage.find('.ui-content').css('width',breite+'px');
+	$.mobile.activePage.find('.ui-content').css('height',hoehe+'px');
 	// $.mobile.activePage.find('.ui-content').css('height',$.mobile.activePage.find('.ui-page').width()+'px');
 }
 function enablescrolling() {
@@ -577,9 +577,9 @@ function enablescrolling() {
 	$.mobile.activePage.parents('body').removeClass('disablescrolling');
 	$.mobile.activePage.parents('container').removeClass('disablescrolling');
 	$.mobile.activePage.parent().find('.ui-page').removeClass('disablescrolling');
-	// $.mobile.activePage.parent().find('.ui-page').removeAttr('style');
 	$.mobile.activePage.find('.ui-content').removeClass('disablescrolling');
-	// $.mobile.activePage.find('.ui-content').removeAttr('style');
+	$.mobile.activePage.parent().find('.ui-page').removeAttr('style');
+	$.mobile.activePage.find('.ui-content').removeAttr('style');
 }
 
 function native_keyboard_manipulation() {
